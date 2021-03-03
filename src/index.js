c21bd856c972a1972f6d05fe0fd759f5c3f3749f
Amit Goswami <cseiubatgoswami@gmail.com>
2021-03-03 21:06:36 +0600

    update2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Cards from './Cards'
+
 import './index.css'
-import sdata from './sdata'
+
+import App from './App'
 
 
-ReactDOM.render(
-  <>
-    <h1 className="heading_style">List of top 5 Netflix Series in 2020</h1>
-
-    {sdata.map((val,index)=>{
-        return(
-          <Cards 
-                key={val.id}
-                imgsrc={val.imgsrc} 
-                title={val.title} 
-                sname={val.sname} 
-                link={val.link} 
-          />
-        )
-    })}
-  
-  </>,
+ReactDOM.render(<App />,
   document.getElementById("root")
 )
